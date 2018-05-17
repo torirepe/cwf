264fba8cc225d5da4dcc72e68eb06edbb22231a9
N-5 <yfe63590@gmail.com>
2018-05-17 14:10:18 +0900

    feat

--- company/index.php
+++ company/index.php
@@ -18,7 +18,7 @@
         <dt class="profile-list__term">社　名</dt>
         <dd class="profile-list__description">株式会社CWファシリティソリューション</dd>
         <dt class="profile-list__term">所在地</dt>
-        <dd class="profile-list__description">〒108-8710　東京都港区港南1丁目8-35　Tel:03-3474-6530</dd>
+        <dd class="profile-list__description">〒108-8710　<br class="is-sp">東京都港区港南1丁目8-35　Tel:03-3474-6530</dd>
         <dt class="profile-list__term">資本金</dt>
         <dd class="profile-list__description">4億1千万円</dd>
         <dt class="profile-list__term">事業開始</dt>
@@ -57,23 +57,25 @@
   
   <section>
     <div class="history">
-      <h2 class="history-heading"></h2>
-      <div class="history-block">
-        <div class="heading">開業</div>
-        <div class="date">2004年（平成16年）4月</div>
-        <div class="text">オフィス構築にかかわる不動産コンサルティングを柱に 千代田区霞が関にて開業</div>
-      </div>
+      <h2 class="history-heading c-sectiontitle"><span>沿革</span></h2>
+      <div class="history-blocks">
+        <div class="history-block">
+          <div class="heading">開業</div>
+          <div class="date">2004年（平成16年）4月</div>
+          <div class="text">オフィス構築にかかわる不動産コンサルティングを柱に 千代田区霞が関にて開業</div>
+        </div>
 
-      <div class="history-block">
-        <div class="heading">吸収合併</div>
-        <div class="date">2016年（平成28年）4月</div>
-        <div class="text">株式会社CWファシリティソリューションを存続会社として<br>ネルソン・スペース・ジェイ株式会社を吸収合併</div>
-      </div>
+        <div class="history-block">
+          <div class="heading">吸収合併</div>
+          <div class="date">2016年（平成28年）4月</div>
+          <div class="text">株式会社CWファシリティソリューションを存続会社として<br>ネルソン・スペース・ジェイ株式会社を吸収合併</div>
+        </div>
 
-      <div class="history-block">
-        <div class="heading">本社移転</div>
-        <div class="date">2017年（平成29年）10月</div>
-        <div class="text">業務開始日　2017年10月23日（月）<br>住所：東京都港区港南１丁目8-35　 〒108-8710</div>
+        <div class="history-block">
+          <div class="heading">本社移転</div>
+          <div class="date">2017年（平成29年）10月</div>
+          <div class="text">業務開始日　2017年10月23日（月）<br>住所：東京都港区港南１丁目8-35　 〒108-8710</div>
+        </div>
       </div>
     </div>
   </section>
---
+++ contact/index.php
@@ -0,0 +1,73 @@
+
+<?php require("../inc/_header.php");?>
+  <main id="contact">
+
+  <!-- hero -->
+  <div class="visual">
+    <h2 class="visual-heading">
+      <span class="hero-title__en">CONTACT</span>
+      <span class="hero-title__en">お問い合わせ</span>
+    </h2>
+  </div>
+  <!-- /hero -->
+
+
+  <!-- form -->
+  <section>
+    <div class="form">
+      <div class="intro">
+        <p class="text">オフィスに関するお問い合わせ、ご質問などはこちらのフォームよりお問い合わせください。<br>また、フォームがうまく送信出来ない場合などは、メールにてお問い合わせください。</p>
+        <p class="caution"><span class="form_required">必須</span>は必須入力項目です</p>
+      </div>
+      <table class="form-list">
+        <tbody>
+          <tr class="from-list_item">
+            <th>お名前<span class="form_required">必須</span></th>
+            <td><input type="text" maxlength="50" placeholder="姓"><input type="text" maxlength="50" placeholder="名"></td>
+          </tr>
+          <tr class="from-list_item">
+            <th>会社名<span class="form_required">必須</span></th>
+            <td><input type="text"></td>
+          </tr>
+          <tr class="from-list_item">
+            <th>部署・役職</th>
+            <td><input type="text"></td>
+          </tr>
+          <tr class="from-list_item">
+            <th>ご住所</th>
+            <td><input type="text"></td>
+          </tr>
+          <tr class="from-list_item">
+            <th>電話番号<span class="form_required">必須</span></th>
+            <td><input type="text"></td>
+          </tr>
+          <tr class="from-list_item">
+            <th>FAX番号</th>
+            <td><input type="text"></td>
+          </tr>
+          <tr class="from-list_item">
+            <th>E-MAIL<span class="form_required">必須</span></th>
+            <td><input type="text"></td>
+          </tr>
+          <tr class="from-list_item">
+            <th>E-MAIL<small>(確認用)</small><span class="form_required">必須</span></th>
+            <td><input type="text"></td>
+          </tr>
+          <tr class="from-list_item">
+            <th>お問い合わせ内容<span class="form_required">必須</span></th>
+            <td><textarea name="" id="" cols="30" rows="10"></textarea></td>
+          </tr>
+        </tbody>
+      </table>
+      <div class="form-check">
+        <div class="heading"><a href="../privacyporicy/">個人情報保護方針 ></a></div>
+        <div class="check"><label><span class="form_required">必須</span><input type="checkbox">上記の内容に同意する　</label></div>
+      </div>
+      <div class="form-btn">
+        <div class="form-btn__confirm"><input type="submit" value="送信"></div>
+      </div>
+    </div>
+  </section>
+  <!-- form -->
+
+<?php require("../inc/_footer.php");?>
