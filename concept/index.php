224faf479531d46568deead1b563e1f9e9230f16
torirepe <t.motoe@amd.tokyo>
2018-05-17 16:52:32 +0900

    追記

--- concept/index.php
+++ concept/index.php
@@ -3,10 +3,10 @@
   <main id="concept">
 
   <!-- hero -->
-  <div class="visual">
-    <h2 class="visual-heading">
-      <span class="hero-title__en">CONCEPT</span>
-      <span class="hero-title__en">コンセプト</span>
+  <div class="visual c-visual">
+    <h2 class="visual-heading c-visual-heading">
+      <span class="visual-heading__en c-visual-heading__en">CONCEPT</span>
+      <span class="visual-heading__jp c-visual-heading__jp">コンセプト</span>
     </h2>
   </div>
   <!-- /hero -->
@@ -16,9 +16,9 @@
   <section>
     <div class="workplace">
       <div class="intro">
-        <div class="intro__copy">関わるすべての人の“勝ち”つながるワークプレイスづくり</div>
+        <div class="intro__copy">関わるすべての人の“勝ち”つながる<br class="is-st">ワークプレイスづくり</div>
         <div class="intro__title">Competitive Workplace</div>
-        <div class="intro__text">すべての人が活き活きと働き活躍している。個人やチームの力が会社の力になって共に成長していく。そんな個人と企業/組織のＷＩＮ-ＷＩＮな“はたらき方”を、ワークプレイスづくりを通して支えていくことが、我々ＣＷＦがクライアントに提供する価値です。</div>
+        <div class="intro__text">すべての人が活き活きと働き活躍している。個人やチームの力が会社の力になって共に成長していく。<br>そんな個人と企業/組織のＷＩＮ-ＷＩＮな“はたらき方”を、ワークプレイスづくりを通して支えていくことが、<br>我々ＣＷＦがクライアントに提供する価値です。</div>
       </div>
       <div class="detail">
         <div class="detail-block">
@@ -26,18 +26,18 @@
             <div class="heading">多様な人材が活躍し企業としても成長する、<br>これからの“はたらき方”を考える</div>
             <div class="text">ワーカーの年齢・家族構成／ライフスタイル等の変化が進んでおり、個人の活躍を維持するには、その多様な働き方のニーズを取り込む必要があります。一方で企業・組織として、新しい価値を生み出すチームとして機能する為には、ビジョンや目的を共有してつながり続ける“はたらき方”が求められます。トレードオフになりがちな２者の関係をバランスさせ、新たな視点から相乗効果の創出を図ります。</div>
           </div>
-          <div class="detail-block__picture">
-            <img src="" alt="">
+          <div class="detail-block__picture is-shadowleft">
+            <img src="../images/concept/workplace_pic_1.jpg" alt="イメージ">
           </div>
         </div>
         
         <div class="detail-block order">
           <div class="detail-block__textbox">
-            <div class="heading">変化する“はたらき方”を受容するワークプレイスを計画する</div>
+            <div class="heading">変化する“はたらき方”を受容する<br>ワークプレイスを計画する</div>
             <div class="text">“はたらき方”が多様化している中で、各企業の働く環境の計画／運用の取り組みは、その変化に対応できていません。在宅やサードプレイス等、“はたらく場所“の選択肢が増え、それらを利用する中でまた更なるニーズやチャンスが生まれ、現在メインに使っているオフィスの役割も変化していきます。時限的な条件や画一的なアイディアに左右されず、ワークプレイスは変化に合せて常に更新するものと考えた計画を行います。</div>
           </div>
-          <div class="detail-block__picture">
-            <img src="" alt="">
+          <div class="detail-block__picture is-shadowright">
+            <img src="../images/concept/workplace_pic_2.jpg" alt="イメージ">
           </div>
         </div>
       </div>
@@ -51,7 +51,7 @@
       <h2 class="approach-heading">ＣＷＦのアプローチ</h2>
       <ul class="approach-list">
         <li class="approach-list_item">
-          <div class="image"></div>
+          <div class="image"><img src="../images/concept/approach_icon_1.svg" alt=""></div>
           <div class="textbox">
             <div class="title">
               <div class="num">01</div>
@@ -61,23 +61,24 @@
           </div>
         </li>
         <li class="approach-list_item">
-          <div class="image"></div>
+          <div class="image"><img src="../images/concept/approach_icon_2.svg" alt=""></div>
           <div class="textbox">
             <div class="title">
-              <div class="num">01</div>
-              <div class="text">企業としての方向性とワーカーの<br>“はたらき方”をフィットさせる</div>
+              <div class="num">02</div>
+              <div class="text">多様な選択肢からワークプレイス<br>づくりの実行シナリオを考える</div>
             </div>
-            <div class="lead">企業としての本質的な課題の解決に向けて、個人やチームのこれからのはたらき方をクライアントと一緒に考えていきます。空間としての計画・デザインに先行して、企業として目指すはたらき方のポイントを明確にして、各ワーカーの具体的な行動につなげていくコンセプトと計画づくりを行います。</div>
+            <div class="lead">目指すワークプレイスの方向性や基本計画も策定しても、その実現に向けては既存の施設での改修や移転、サードプレイスの活用などの様々な選択肢があります。単年度の視点だけでなく段階的な移行をふまえたシナリオを考えながら実行計画を立案します。</div>
           </div>
         </li>
         <li class="approach-list_item">
-          <div class="image"></div>
+          <div class="image"><img src="../images/concept/approach_icon_3.svg" alt=""></div>
           <div class="textbox">
             <div class="title">
-              <div class="num">01</div>
-              <div class="text">企業としての方向性とワーカーの<br>“はたらき方”をフィットさせる</div>
+              <div class="num">03</div>
+              <div class="text">専門家としてクライアントの目的の<br>実現によりそうプロジェクトサポート</div>
             </div>
-            <div class="lead">企業としての本質的な課題の解決に向けて、個人やチームのこれからのはたらき方をクライアントと一緒に考えていきます。空間としての計画・デザインに先行して、企業として目指すはたらき方のポイントを明確にして、各ワーカーの具体的な行動につなげていくコンセプトと計画づくりを行います。</div>
+            <div class="lead">ワークプレイスは構築段階でも、ビルオーナー、工事会社等と多岐にわたる交渉、<br>調整も必要です。スケジュールやコストの管理だけでなく、計画段階で意図したことを最終的な工事の完成や運用計画づくりに反映させるところまでプロフェッショナルとしてサポートいたします。
+            </div>
           </div>
         </li>
       </ul>
@@ -89,8 +90,14 @@
   <!-- office -->
   <section>
     <div class="office">
-      <div class="office-block"></div>
-      <div class="office-block"></div>
+      <div class="office-block1">
+        <img class="is-tp" src="../images/concept/office_1.png" alt="">
+        <img class="is-sp" src="../images/concept/office_1_sp.png" alt="">
+      </div>
+      <div class="office-block2">
+        <img class="is-tp" src="../images/concept/office_2.png" alt="">
+        <img class="is-sp" src="../images/concept/office_2_sp.png" alt="">
+      </div>
     </div>
   </section>
   <!-- office -->
