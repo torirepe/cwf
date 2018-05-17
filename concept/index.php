30946be03448e99f0009dcb6e6cb21f41dc1106a
torirepe <t.motoe@amd.tokyo>
2018-05-17 17:36:46 +0900

    visual

--- concept/index.php
+++ concept/index.php
@@ -23,7 +23,7 @@
       <div class="detail">
         <div class="detail-block">
           <div class="detail-block__textbox">
-            <div class="heading">多様な人材が活躍し企業としても成長する、<br>これからの“はたらき方”を考える</div>
+            <div class="heading">多様な人材が活躍し<br class="is-st">企業としても成長する、<br>これからの“はたらき方”を考える</div>
             <div class="text">ワーカーの年齢・家族構成／ライフスタイル等の変化が進んでおり、個人の活躍を維持するには、その多様な働き方のニーズを取り込む必要があります。一方で企業・組織として、新しい価値を生み出すチームとして機能する為には、ビジョンや目的を共有してつながり続ける“はたらき方”が求められます。トレードオフになりがちな２者の関係をバランスさせ、新たな視点から相乗効果の創出を図ります。</div>
           </div>
           <div class="detail-block__picture is-shadowleft">
@@ -33,7 +33,7 @@
         
         <div class="detail-block order">
           <div class="detail-block__textbox">
-            <div class="heading">変化する“はたらき方”を受容する<br>ワークプレイスを計画する</div>
+            <div class="heading">変化する“はたらき方”を<br class="is-st">受容する<br class="is-pc">ワークプレイスを計画する</div>
             <div class="text">“はたらき方”が多様化している中で、各企業の働く環境の計画／運用の取り組みは、その変化に対応できていません。在宅やサードプレイス等、“はたらく場所“の選択肢が増え、それらを利用する中でまた更なるニーズやチャンスが生まれ、現在メインに使っているオフィスの役割も変化していきます。時限的な条件や画一的なアイディアに左右されず、ワークプレイスは変化に合せて常に更新するものと考えた計画を行います。</div>
           </div>
           <div class="detail-block__picture is-shadowright">
@@ -55,7 +55,7 @@
           <div class="textbox">
             <div class="title">
               <div class="num">01</div>
-              <div class="text">企業としての方向性とワーカーの<br>“はたらき方”をフィットさせる</div>
+              <div class="text">企業としての方向性と<br class="is-st">ワーカーの“はたらき方”を<br class="is-st">フィットさせる</div>
             </div>
             <div class="lead">企業としての本質的な課題の解決に向けて、個人やチームのこれからのはたらき方をクライアントと一緒に考えていきます。空間としての計画・デザインに先行して、企業として目指すはたらき方のポイントを明確にして、各ワーカーの具体的な行動につなげていくコンセプトと計画づくりを行います。</div>
           </div>
@@ -65,7 +65,7 @@
           <div class="textbox">
             <div class="title">
               <div class="num">02</div>
-              <div class="text">多様な選択肢からワークプレイス<br>づくりの実行シナリオを考える</div>
+              <div class="text">多様な選択肢から<br class="is-st">ワークプレイス<br class="is-pc">づくりの<br class="is-st">実行シナリオを考える</div>
             </div>
             <div class="lead">目指すワークプレイスの方向性や基本計画も策定しても、その実現に向けては既存の施設での改修や移転、サードプレイスの活用などの様々な選択肢があります。単年度の視点だけでなく段階的な移行をふまえたシナリオを考えながら実行計画を立案します。</div>
           </div>
@@ -75,7 +75,7 @@
           <div class="textbox">
             <div class="title">
               <div class="num">03</div>
-              <div class="text">専門家としてクライアントの目的の<br>実現によりそうプロジェクトサポート</div>
+              <div class="text">専門家としてクライアントの目的の<br class="is-pc">実現によりそうプロジェクトサポート</div>
             </div>
             <div class="lead">ワークプレイスは構築段階でも、ビルオーナー、工事会社等と多岐にわたる交渉、<br>調整も必要です。スケジュールやコストの管理だけでなく、計画段階で意図したことを最終的な工事の完成や運用計画づくりに反映させるところまでプロフェッショナルとしてサポートいたします。
             </div>
